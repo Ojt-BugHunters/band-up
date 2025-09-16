9143cb00cb07340968642e5901b6fda7824fe5ef
Võ Hoàng Minh <minh7n3@gmail.com>
2025-09-16 14:04:55 +0700

    init backend

---
+++ backend/src/main/java/com/project/Band_Up/BandUpApplication.java
@@ -0,0 +1,13 @@
+package com.project.Band_Up;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BandUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BandUpApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/project/Band_Up/BandUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.Band_Up;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BandUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
