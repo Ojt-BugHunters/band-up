3c8093031bd050b8c3d56acfbf35945f6a7bb171
Nhật Anh <chip@Nhats-MacBook-Pro.local>
2025-09-17 14:48:05 +0700

    feat: resolve login page

--- frontend/src/app/auth/login/page.tsx
+++ frontend/src/app/auth/login/page.tsx
@@ -1,11 +1,177 @@
-import { LoginForm } from "@/app/auth/login/login-form"
+/* eslint-disable @next/next/no-img-element */
+'use client';
+import { cn } from '@/lib/utils';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent } from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { FiEye, FiEyeOff } from 'react-icons/fi';
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from '@/components/ui/form';
+import { useLoginForm } from '@/app/hooks/use-login-form';
+import { FaFacebook, FaGoogle } from 'react-icons/fa';
+import { useState } from 'react';
 
 export default function LoginPage() {
-  return (
-    <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
-      <div className="w-full max-w-sm md:max-w-3xl">
-        <LoginForm />
-      </div>
-    </div>
-  )
+    return (
+        <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+            <div className="w-full max-w-sm md:max-w-3xl">
+                <LoginForm />
+            </div>
+        </div>
+    );
+}
+
+function LoginForm({ className, ...props }: React.ComponentProps<'div'>) {
+    const { form, onSubmit } = useLoginForm();
+    const [showPassword, setShowPassword] = useState(false);
+    return (
+        <div className={cn('flex flex-col gap-6', className)} {...props}>
+            <Card className="overflow-hidden p-0">
+                <CardContent className="grid p-0 md:grid-cols-2">
+                    <Form {...form}>
+                        <form className="p-6 md:p-8" onSubmit={onSubmit}>
+                            <div className="flex flex-col gap-6">
+                                <div className="flex flex-col items-center text-center">
+                                    <h1 className="text-2xl font-bold">
+                                        Welcome back
+                                    </h1>
+                                    <p className="text-muted-foreground text-balance">
+                                        Login to your BandUp account
+                                    </p>
+                                </div>
+                                <FormField
+                                    control={form.control}
+                                    name="email"
+                                    render={({ field }) => (
+                                        <FormItem className="grid gap-2">
+                                            <FormLabel>Email</FormLabel>
+                                            <FormControl>
+                                                <Input
+                                                    {...field}
+                                                    required
+                                                    placeholder="johndoe@example.com"
+                                                />
+                                            </FormControl>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
+                                <FormField
+                                    control={form.control}
+                                    name="password"
+                                    render={({ field }) => (
+                                        <FormItem className="grid gap-3">
+                                            <div className="flex items-center">
+                                                <FormLabel htmlFor="password">
+                                                    Password
+                                                </FormLabel>
+                                                <a
+                                                    href="#"
+                                                    className="ml-auto text-sm underline-offset-2 hover:underline"
+                                                >
+                                                    Forgot your password?
+                                                </a>
+                                            </div>
+                                            <FormControl>
+                                                <div className="relative">
+                                                    <Input
+                                                        id="password"
+                                                        type={
+                                                            showPassword
+                                                                ? 'text'
+                                                                : 'password'
+                                                        }
+                                                        {...field}
+                                                        className="pr-10"
+                                                    />
+                                                    <button
+                                                        type="button"
+                                                        onClick={() =>
+                                                            setShowPassword(
+                                                                (prev) => !prev,
+                                                            )
+                                                        }
+                                                        className="text-muted-foreground absolute top-1/2 right-2 -translate-y-1/2"
+                                                        tabIndex={-1}
+                                                    >
+                                                        {showPassword ? (
+                                                            <FiEyeOff className="h-5 w-5" />
+                                                        ) : (
+                                                            <FiEye className="h-5 w-5" />
+                                                        )}
+                                                    </button>
+                                                </div>
+                                            </FormControl>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
+                                <Button
+                                    type="submit"
+                                    className="w-full cursor-pointer"
+                                >
+                                    Login
+                                </Button>
+                                <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
+                                    <span className="bg-card text-muted-foreground relative z-10 px-2">
+                                        Or continue with
+                                    </span>
+                                </div>
+                                <div className="grid grid-cols-2 gap-4">
+                                    <Button
+                                        variant="outline"
+                                        type="button"
+                                        className="w-full cursor-pointer"
+                                    >
+                                        <FaGoogle className="text-red-500" />
+                                        <span className="sr-only">
+                                            Login with Google
+                                        </span>
+                                    </Button>
+                                    <Button
+                                        variant="outline"
+                                        type="button"
+                                        className="w-full cursor-pointer"
+                                    >
+                                        <FaFacebook className="text-2xl text-blue-600" />
+                                        <span className="sr-only">
+                                            Login with Meta
+                                        </span>
+                                    </Button>
+                                </div>
+                                <div className="text-center text-sm">
+                                    Don&apos;t have an account?{' '}
+                                    <a
+                                        href="#"
+                                        className="underline underline-offset-4"
+                                    >
+                                        Sign up
+                                    </a>
+                                </div>
+                            </div>
+                        </form>
+                    </Form>
+
+                    <div className="bg-muted relative hidden md:block">
+                        <img
+                            src="/loginimg.jpg"
+                            alt="Image"
+                            className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+                        />
+                    </div>
+                </CardContent>
+            </Card>
+            <div className="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
+                By clicking continue, you agree to our{' '}
+                <a href="#">Terms of Service</a> and{' '}
+                <a href="#">Privacy Policy</a>.
+            </div>
+        </div>
+    );
 }
