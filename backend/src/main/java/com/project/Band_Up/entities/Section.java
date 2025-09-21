ffde068dddd04f2f0fa23b0da2868710200551c0
Võ Hoàng Minh <minh7n3@gmail.com>
2025-09-22 04:54:50 +0700

    feat: add entity Section

---
+++ backend/src/main/java/com/project/Band_Up/entities/Section.java
@@ -0,0 +1,37 @@
+package com.project.Band_Up.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.util.Map;
+import java.util.UUID;
+
+@AllArgsConstructor
+@Builder
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "section")
+public class Section {
+    @Id
+    @GeneratedValue
+    @UuidGenerator(style = UuidGenerator.Style.TIME)
+    private UUID id;
+
+    @ManyToOne (fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "test_id", nullable = false, foreignKey = @ForeignKey(name = "fk_section_test"))
+    private Test test;
+
+    @NotNull
+    private String title;
+
+    @NotNull
+    private Integer oderInDex;
+
+    // JSON metadata
+    @Column(columnDefinition = "jsonb")
+    private Map<String, Object> metadata;
+}
