39ec557ea8d1817a3a8cd05eb399f91646d8883f
Đặng Minh Hoàng <Hoangsusp@gmail.com>
2025-09-23 16:49:58 +0700

    Added Jwt Filter with Spring Security Context

--- backend/src/main/java/com/project/Band_Up/filters/JwtFilter.java
+++ backend/src/main/java/com/project/Band_Up/filters/JwtFilter.java
@@ -1,17 +1,69 @@
 package com.project.Band_Up.filters;
 
+import com.project.Band_Up.entities.RefreshToken;
+import com.project.Band_Up.repositories.RefreshTokenRepository;
+import com.project.Band_Up.utils.JwtUtil;
+import io.jsonwebtoken.JwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseCookie;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.UUID;
 
+@Component
 public class JwtFilter extends OncePerRequestFilter {
 
+    @Autowired
+    private JwtUtil jwtUtil;
+    @Autowired
+    private RefreshTokenRepository refreshTokenRepository;
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+        Cookie[] cookies = request.getCookies();
+        String accessToken = null;
+        String refreshToken = null;
+        UUID accountId = null;
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if ("AccessToken".equals(cookie.getName())) {
+                    accessToken = cookie.getValue();
+                } else if ("RefreshToken".equals(cookie.getName())) {
+                    refreshToken = cookie.getValue();
+                }
+            }
+        }
+        try {
+            if (accessToken != null) {
+                accountId = UUID.fromString(jwtUtil.extractSubject(accessToken));
+            } else if (refreshToken != null) {
+                accountId = jwtUtil.validateRefreshToken(refreshToken);
+
+                ResponseCookie newAccessToken = jwtUtil.getAccessTokenCookie(accountId);
+
+                response.addHeader("Set-Cookie", newAccessToken.toString());
+            }
+
+            if (accountId != null) {
+                Authentication authentication = jwtUtil.getAuthentication(accountId);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+
+        } catch (JwtException ex) {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid or expired token");
+            return;
+        }
         filterChain.doFilter(request,response);
     }
 }
