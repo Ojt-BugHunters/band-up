76fd40552aacf0da08c237d58d2a9ff399ac5956
namdang-fdp <namdangcoder@gmail.com>
2025-09-24 21:33:08 +0700

    feat: receive params and render by condition

--- frontend/src/app/test/[skill]/[id]/do/page.tsx
+++ frontend/src/app/test/[skill]/[id]/do/page.tsx
@@ -1,9 +1,43 @@
 import { ReadingTest } from '@/components/reading-test';
+import { SpeakingTest } from '@/components/speaking-test';
+import { WritingTest } from '@/components/writing-test';
+import { ListeningTest } from '@/components/listening-test';
 
-export default function Home() {
-    return (
-        <main className="bg-background min-h-screen">
-            <ReadingTest />
-        </main>
-    );
+type DoTestProps = {
+    searchParams: {
+        mode?: string;
+        skill?: string;
+        section?: string | string[];
+    };
+};
+
+export default function DoTestPage({ searchParams }: DoTestProps) {
+    const { mode, skill, section } = searchParams;
+
+    let sections: string[] = [];
+    if (Array.isArray(section)) {
+        sections = section;
+    } else if (typeof section === 'string') {
+        sections = section.split(',');
+    }
+
+    let Component: React.ReactNode = null;
+    switch (skill) {
+        case 'reading':
+            Component = <ReadingTest mode={mode} sections={sections} />;
+            break;
+        case 'listening':
+            Component = <ListeningTest mode={mode} sections={sections} />;
+            break;
+        case 'writing':
+            Component = <WritingTest mode={mode} />;
+            break;
+        case 'speaking':
+            Component = <SpeakingTest mode={mode} />;
+            break;
+        default:
+            Component = <div>Vui lòng chọn kỹ năng hợp lệ</div>;
+    }
+
+    return <main className="bg-background min-h-screen">{Component}</main>;
 }
