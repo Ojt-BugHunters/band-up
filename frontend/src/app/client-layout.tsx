167a1c2cd7e52bc37b4f6ad249f46c72808c2455
namdang-fdp <namdangcoder@gmail.com>
2025-09-25 00:25:36 +0700

    chore: reformat

--- frontend/src/app/client-layout.tsx
+++ frontend/src/app/client-layout.tsx
@@ -1,21 +1,9 @@
 'use client';
 
-import { Geist, Geist_Mono } from 'next/font/google';
-import { ThemeProvider } from 'next-themes';
 import { usePathname } from 'next/navigation';
 import { Header } from '@/components/header';
 import { Footer } from '@/components/footer';
 import { Toaster } from 'sonner';
-
-const geistSans = Geist({
-    variable: '--font-geist-sans',
-    subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-    variable: '--font-geist-mono',
-    subsets: ['latin'],
-});
 
 export default function ClientLayout({
     children,
@@ -25,18 +13,11 @@ export default function ClientLayout({
     const pathname = usePathname();
     const hideLayout = pathname.includes('/do');
     return (
-        <html
-            lang="en"
-            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
-            <body>
-                <ThemeProvider>
-                    {!hideLayout && <Header />}
-                    {children}
-                    {!hideLayout && <Footer />}
-                    <Toaster richColors />
-                </ThemeProvider>
-            </body>
-        </html>
+        <>
+            {!hideLayout && <Header />}
+            {children}
+            {!hideLayout && <Footer />}
+            <Toaster richColors />
+        </>
     );
 }
--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,11 +1,23 @@
 import type { Metadata } from 'next';
 import ClientLayout from './client-layout';
 import QueryProvider from './query-provider';
+import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+
 export const metadata: Metadata = {
     title: 'BandUp',
     description: 'BandUp Ielts Learning App',
 };
+
+const geistSans = Geist({
+    variable: '--font-geist-sans',
+    subsets: ['latin'],
+});
+
+const geistMono = Geist_Mono({
+    variable: '--font-geist-mono',
+    subsets: ['latin'],
+});
 
 export default function RootLayout({
     children,
@@ -13,8 +25,15 @@ export default function RootLayout({
     children: React.ReactNode;
 }) {
     return (
-        <QueryProvider>
-            <ClientLayout>{children}</ClientLayout>
-        </QueryProvider>
+        <html
+            lang="en"
+            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+            <body>
+                <QueryProvider>
+                    <ClientLayout>{children}</ClientLayout>
+                </QueryProvider>
+            </body>
+        </html>
     );
 }
