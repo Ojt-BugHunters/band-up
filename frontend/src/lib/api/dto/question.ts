cf5ac0068336b85ba08c86b944da9e3506fef69d
namdang-fdp <namdangcoder@gmail.com>
2025-09-25 11:55:03 +0700

    feat: add listening dto

--- frontend/src/lib/api/dto/question.ts
+++ frontend/src/lib/api/dto/question.ts
@@ -1,8 +1,39 @@
-export interface Question {
+export type ReadingQuestionType =
+    | 'multiple-choice'
+    | 'short-answer'
+    | 'true-false'
+    | 'completion';
+
+export type ListeningQuestionType = 'multiple-choice' | 'completion';
+
+export interface ReadingQuestion {
     id: number;
-    type: 'multiple-choice' | 'short-answer' | 'true-false' | 'completion';
+    type: ReadingQuestionType;
     question: string;
     options?: string[];
     answer: string;
     image?: string;
+}
+
+export interface ListeningQuestion {
+    id: number;
+    type: ListeningQuestionType;
+    question: string;
+    option?: string[];
+    image?: string;
+}
+
+export interface Passage {
+    id: string;
+    title: string;
+    content: string;
+    questions: ReadingQuestion[];
+}
+
+export interface ListeningSection {
+    id: string;
+    title: string;
+    audioUrl: string;
+    duration: number;
+    question: ListeningQuestion[];
 }
