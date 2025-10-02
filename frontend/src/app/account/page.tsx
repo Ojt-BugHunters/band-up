e6f9da19899f26ddeb2c03f454b492722a3ce081
namdang-fdp <namdangcoder@gmail.com>
2025-10-02 13:14:25 +0700

    feat: add account profile view page

---
+++ frontend/src/app/account/page.tsx
@@ -0,0 +1,11 @@
+export default function ViewProfilePage() {
+    return (
+        <div className="mx-5 my-8">
+            <div className="mx-auto max-w-6xl space-y-6">
+                <div className="relative">
+                    <div className="overflow-hidden bg-white shadow-md"></div>
+                </div>
+            </div>
+        </div>
+    );
+}
