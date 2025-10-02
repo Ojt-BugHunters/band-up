e6a0586a72c193da862fea081c63ec7ccd3197e0
namdang-fdp <namdangcoder@gmail.com>
2025-10-02 11:16:22 +0700

    feat: add auto refresh token hook

---
+++ frontend/src/hooks/use-refresh-token.ts
@@ -0,0 +1,23 @@
+import { fetchWrapper, throwIfError } from '@/lib/api';
+import { useQuery } from '@tanstack/react-query';
+import { toast } from 'sonner';
+
+export const useRefreshToken = () => {
+    return useQuery({
+        queryKey: ['refresh-token'],
+        queryFn: async () => {
+            const response = await fetchWrapper('/auth/refresh', {
+                method: 'POST',
+            });
+            await throwIfError(response);
+            toast.success('Refresh token successfull');
+            return response;
+        },
+        refetchInterval: 10 * 1000,
+        refetchIntervalInBackground: true,
+        retry: false,
+        meta: {
+            onError: (error: any) => toast.error(error.message),
+        },
+    });
+};
