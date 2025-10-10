b789f4bfd73bbbadf410a4cd7306013020245c9d
namdang-fdp <namdangcoder@gmail.com>
2025-10-10 09:29:18 +0700

    feat: add hook connect to api reset password

--- frontend/src/hooks/use-reset-password.ts
+++ frontend/src/hooks/use-reset-password.ts
@@ -7,27 +7,40 @@ import { toast } from 'sonner';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 
-export const resetPasswordSchema = z.object({
-    email: z.string().min(1, { message: 'Email must not be empty' }),
-    password: passwordSchema,
-});
+export const resetPasswordSchema = z
+    .object({
+        password: passwordSchema,
+        confirmPassword: z.string(),
+    })
+    .refine((data) => data.password === data.confirmPassword, {
+        message: 'Passwords do not match',
+        path: ['confirmPassword'],
+    });
 
-type ResetPasswordFormValues = z.infer<typeof resetPasswordSchema>;
+export type ResetPasswordFormValues = z.infer<typeof resetPasswordSchema>;
+
+type MutateVars = {
+    email: string;
+    password: string;
+};
 
 export const useResetPassword = (inputOtp: string) => {
     const router = useRouter();
 
     const mutation = useMutation({
-        mutationFn: async (values: ResetPasswordFormValues) => {
+        mutationFn: async (values: MutateVars) => {
             const response = await fetchWrapper(
-                `/auth/account/reset-password?${inputOtp}`,
+                `/auth/account/reset-password?inputOtp=${inputOtp}`,
                 {
                     method: 'POST',
                     headers: {
                         Accept: 'application/json',
                         'Content-Type': 'application/json',
                     },
-                    body: JSON.stringify(values),
+                    body: JSON.stringify({
+                        email: values.email,
+                        password: values.password,
+                    }),
                 },
             );
             await throwIfError(response);
