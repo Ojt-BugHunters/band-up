042b86700088c74204517af62eb6d2985b7764e0
namdang-fdp <namdangcoder@gmail.com>
2025-10-12 14:16:13 +0700

    feat: add blog sidebar

---
+++ frontend/src/components/blog-sidebar.tsx
@@ -0,0 +1,29 @@
+import { Bookmark, Heart, Home, Tag } from 'lucide-react';
+import { Button } from './ui/button';
+
+const blogSidebarItems = [
+    { icon: Home, label: 'Home', active: true },
+    { icon: Tag, label: 'Tags' },
+    { icon: Bookmark, label: 'Bookmark List' },
+    {
+        icon: Heart,
+        label: 'Liked Posts',
+    },
+];
+
+export default function BlogSideBar() {
+    return (
+        <nav className="sticky top-20 space-y-2">
+            {blogSidebarItems.map((item) => (
+                <Button
+                    key={item.label}
+                    variant={item.active ? 'secondary' : 'ghost'}
+                    className="hover:bg-accent h-11 w-full justify-start gap-3 transition-all duration-200"
+                >
+                    <item.icon className="h-5 w-5" />
+                    <span className="text-sm font-medium">{item.label}</span>
+                </Button>
+            ))}
+        </nav>
+    );
+}
