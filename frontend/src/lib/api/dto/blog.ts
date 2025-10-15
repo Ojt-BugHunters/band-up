e41dbb96a4c0c3aa491139cbbc128e45230d760e
namdang-fdp <namdangcoder@gmail.com>
2025-10-15 08:55:40 +0700

    feat: blog dto and sample data for blog

--- frontend/src/lib/api/dto/blog.ts
+++ frontend/src/lib/api/dto/blog.ts
@@ -1,22 +1,43 @@
+import { Tag } from './category';
+
+export type ReactType = 'like' | 'love' | 'sad' | 'angry' | 'haha' | 'wow';
+
+export interface BlogReact {
+    id: string;
+    reactAuthor: Author;
+    reactType: ReactType;
+}
+
 export interface Author {
     id: string;
     name: string;
-    avatar: string;
+    avatar?: string;
 }
 
-export interface Tag {
-    id: string;
-    name: string;
-}
-
-export interface Post {
+export interface BlogPost {
     id: string;
     author: Author;
     title: string;
+    titleImg?: string;
     content: string;
-    coverImage: string;
-    tag: Tag[];
-    reactions: number;
-    comments: number;
-    publishedAt: string;
+    tags: Tag[];
+    numberOfReaders: number;
+    numberOfComments: number;
+    publishedDate: string;
+    reacts?: BlogReact[];
+}
+
+export interface BlogPostDetail {
+    blogPost: BlogPost;
+    content: string;
+}
+
+export interface FeatureBlogs {
+    id: string;
+    title: string;
+    subContent: string;
+    tags: Tag[];
+    image: string;
+    numberOfReader: number;
+    numberOfComments: number;
 }
