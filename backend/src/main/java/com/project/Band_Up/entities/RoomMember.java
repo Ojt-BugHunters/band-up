23b189dc919d370febf0422a03389999f0c3b84b
Võ Hoàng Minh <minh7n3@gmail.com>
2025-11-06 16:15:25 +0700

    feat: create entity Room and RoomMember

--- backend/src/main/java/com/project/Band_Up/entities/RoomMember.java
+++ backend/src/main/java/com/project/Band_Up/entities/RoomMember.java
@@ -1,4 +1,43 @@
 package com.project.Band_Up.entities;
 
+import com.project.Band_Up.enums.Role;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.UuidGenerator;
+import org.joda.time.DateTime;
+
+import java.util.UUID;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "room_members")
 public class RoomMember {
+    @Id
+    @GeneratedValue
+    @UuidGenerator(style = UuidGenerator.Style.TIME)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false,
+            foreignKey = @ForeignKey(name = "fk_room_member_user"))
+    private Account user;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name= "room_id", nullable = false,
+            foreignKey = @ForeignKey(name = "fk_room_member_room"))
+    private Room room;
+
+    @NotNull
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    private DateTime joinedAt;
+
+    private Boolean isActive = true;
 }
