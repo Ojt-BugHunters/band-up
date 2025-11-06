9acda8c7fda4601a9bcf46542b7439f0e6cd95af
Võ Hoàng Minh <minh7n3@gmail.com>
2025-11-06 17:31:15 +0700

    feat: creat dto CreateResponse for Room

--- backend/src/main/java/com/project/Band_Up/dtos/room/RoomCreateRequest.java
+++ backend/src/main/java/com/project/Band_Up/dtos/room/RoomCreateRequest.java
@@ -1,4 +1,16 @@
 package com.project.Band_Up.dtos.room;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class RoomCreateRequest {
+    private String roomName;
+    private String description;
+    private boolean isPrivate;
 }
