1c0d78d0e42bfa06a1e211f4075b502cfbf0d005
Võ Hoàng Minh <minh7n3@gmail.com>
2025-11-06 16:18:11 +0700

    feat: create entity StudySession

--- backend/src/main/java/com/project/Band_Up/entities/StudySession.java
+++ backend/src/main/java/com/project/Band_Up/entities/StudySession.java
@@ -1,4 +1,61 @@
 package com.project.Band_Up.entities;
 
+import com.project.Band_Up.enums.SessionMode;
+import com.project.Band_Up.enums.Status;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Cleanup;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UuidGenerator;
+import org.joda.time.DateTime;
+
+import java.math.BigInteger;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class StudySession {
+    @Id
+    @GeneratedValue
+    @UuidGenerator(style = UuidGenerator.Style.TIME)
+    private String id;
+
+    @ManyToOne(fetch = jakarta.persistence.FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false,
+            foreignKey = @ForeignKey(name = "fk_study_session_user"))
+    private Account user;
+
+    @NotNull
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private SessionMode mode;
+
+    @NotNull
+    @Column(nullable = false)
+    private BigInteger focusTime;
+    @NotNull
+    @Column(nullable = false)
+    private BigInteger shortBreak;
+    @NotNull
+    @Column(nullable = false)
+    private BigInteger longBreak;
+    @NotNull
+    @Column(nullable = false)
+    private Integer cycles;
+
+    private DateTime startedAt;
+    private DateTime endedAt;
+    private BigInteger totalFocusTime;
+
+    @NotNull
+    @Column(nullable = false)
+    private Status status;
+
+    @CreationTimestamp
+    @Column(name = "create_at", nullable = false, updatable = false)
+    private DateTime createAt;
 }
