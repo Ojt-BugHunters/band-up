c9da32a940d9ef919671f920403d8e685469b7cb
Võ Hoàng Minh <minh7n3@gmail.com>
2025-11-06 19:34:36 +0700

    feat: create repo for room and roomMember with crud query

--- backend/src/main/java/com/project/Band_Up/repositories/RoomMemberRepository.java
+++ backend/src/main/java/com/project/Band_Up/repositories/RoomMemberRepository.java
@@ -1,4 +1,10 @@
 package com.project.Band_Up.repositories;
 
-public interface RoomMemberRepository {
+import com.project.Band_Up.entities.RoomMember;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface RoomMemberRepository extends JpaRepository<RoomMember, UUID> {
+
 }
