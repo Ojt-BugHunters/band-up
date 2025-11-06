9f88d74092e87c02bbc77ecbc7321779699e0a9d
Võ Hoàng Minh <minh7n3@gmail.com>
2025-11-06 17:31:55 +0700

    feat: create entity Friend

--- backend/src/main/java/com/project/Band_Up/entities/Friend.java
+++ backend/src/main/java/com/project/Band_Up/entities/Friend.java
@@ -1,4 +1,47 @@
 package com.project.Band_Up.entities;
 
+import com.project.Band_Up.enums.Status;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.UuidGenerator;
+import org.joda.time.DateTime;
+
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "friends", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"user_id", "friend_id"})
+})
 public class Friend {
+    @Id
+    @GeneratedValue
+    @UuidGenerator(style = UuidGenerator.Style.TIME)
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false,
+            foreignKey = @ForeignKey(name = "fk_friend_user"))
+    private Account user;
+
+    @ManyToOne
+    @JoinColumn(name = "friend_id", nullable = false,
+            foreignKey = @ForeignKey(name = "fk_friend_friend"))
+    private Account friend;
+
+    @NotNull
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    private DateTime requestedAt;
+    private DateTime acceptedAt;
+
 }
