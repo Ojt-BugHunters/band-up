09d0355f18ff53b8c62b46ceceb08d2ab877c6cc
Võ Hoàng Minh <minh7n3@gmail.com>
2025-11-06 21:14:11 +0700

    feat: add repository for roomMember

--- backend/src/main/java/com/project/Band_Up/repositories/RoomMemberRepository.java
+++ backend/src/main/java/com/project/Band_Up/repositories/RoomMemberRepository.java
@@ -1,10 +1,14 @@
 package com.project.Band_Up.repositories;
 
+import com.project.Band_Up.entities.Room;
 import com.project.Band_Up.entities.RoomMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface RoomMemberRepository extends JpaRepository<RoomMember, UUID> {
+    int countByRoom(Room room);
 
+    List<RoomMember> findByRoom(Room room);
 }
