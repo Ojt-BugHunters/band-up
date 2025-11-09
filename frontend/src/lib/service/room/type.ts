82951305ecd185b92b11cf1415eea73d1c03b58e
namdang-fdp <namdangcoder@gmail.com>
2025-11-09 22:36:03 +0700

    feat: add api connect to create room

---
+++ frontend/src/lib/service/room/type.ts
@@ -0,0 +1,70 @@
+import { Author } from '@/lib/api/dto/blog';
+import z from 'zod';
+
+export interface Task {
+    id: string;
+    text: string;
+    completed: boolean;
+}
+
+export interface AmbientSound {
+    id: string;
+    name: string;
+    icon: string;
+    category: 'weather' | 'nature' | 'ambiance' | 'workspace';
+    enabled: boolean;
+    volume: number;
+}
+
+export interface LeaderboardUser {
+    rank: number;
+    username: string;
+    avatar: string;
+    studyTime: string;
+    rankChange?: 'up' | 'down' | null;
+    country?: string;
+    status?: string;
+}
+
+export interface RoomInfo {
+    roomName: string;
+    roomOwner: Author;
+}
+
+export type PomodoroPreset = {
+    name: string;
+    focus: number;
+    shortBreak: number;
+    longBreak: number;
+};
+
+export interface RoomMember {
+    roomId: string;
+    userId: string;
+    role: string;
+    joinedAt: string;
+}
+
+export interface Room {
+    id: string;
+    roomName: string;
+    description: string;
+    roomCode: string;
+    isPrivate: boolean;
+    createdBy: string;
+    numberOfMembers: number;
+    createdAt: string;
+    members: RoomMember[];
+}
+
+export const RoomSchema = z.object({
+    roomName: z.string().max(50, 'Max length of room name is 50 characters'),
+    description: z
+        .string()
+        .transform((v) =>
+            v?.trim().length ? v : 'A cozy room for learning and working',
+        ),
+    private: z.boolean(),
+});
+
+export type CreateRoomFormValues = z.infer<typeof RoomSchema>;
