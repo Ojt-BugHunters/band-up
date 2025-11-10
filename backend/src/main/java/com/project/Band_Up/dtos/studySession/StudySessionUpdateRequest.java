ca8f51631ef46f9c46d44dbbe38fe8538cb6df6f
Võ Hoàng Minh <minh7n3@gmail.com>
2025-11-10 20:19:25 +0700

    feat: add service startInterval, endInterval, pingInterval

--- backend/src/main/java/com/project/Band_Up/dtos/studySession/StudySessionUpdateRequest.java
+++ backend/src/main/java/com/project/Band_Up/dtos/studySession/StudySessionUpdateRequest.java
@@ -1,4 +1,21 @@
 package com.project.Band_Up.dtos.studySession;
 
+import com.project.Band_Up.enums.Status;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigInteger;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class StudySessionUpdateRequest {
+    private LocalDateTime startedAt;
+    private LocalDateTime endedAt;
+    private Status status;
+    private BigInteger totalFocusTime;
 }
