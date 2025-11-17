7604e262fa12c611989d90194e1c348dd28d1c1e
Võ Hoàng Minh <minh7n3@gmail.com>
2025-11-17 19:08:04 +0700

    feat: add ActivitiesSummaryDto

---
+++ backend/src/main/java/com/project/Band_Up/dtos/studySession/ActivitiesSummaryDto.java
@@ -0,0 +1,18 @@
+package com.project.Band_Up.dtos.studySession;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ActivitiesSummaryDto {
+    private Integer totalSessions;
+    private Integer focusedTime;
+    private Integer bestSession;
+    private Integer taskCompleted;
+
+}
